dfebee54ff4bf3c901ca835f11073a32bb9c4d24
anikamorris <anikamorris11@gmail.com>
2020-06-15 11:04:04 -0700

    parsed JSON result from locations.json

--- FilmLocations/ViewController.swift
+++ FilmLocations/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getDataFromFile("locations")
     }
 
+    func getDataFromFile(_ fileName:String) {
+        let path = Bundle.main.path(forResource: fileName, ofType: ".json")
+        if let path = path {
+          let url = URL(fileURLWithPath: path)
+          let contents = try? Data(contentsOf: url)
+          do {
+            if let data = contents,
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:Any]] {
+              print(jsonResult)
+            }
+          } catch {
+            print("Error deserializing JSON: \(error)")
+          }
+        }
+        
+    }
 
 }
 
